2460e48f15a8175281db44b4725d1958d26d0e56
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-08-12 14:18:29 -0400

    payment section using stripe

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,14 +1,13 @@
-import { Box } from "@mui/material";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
+import { Box, Alert, AlertTitle } from "@mui/material";
+
 
 const Confirmation = () => {
   return (
     <Box m="90px auto" width="80%" height="50vh">
       <Alert severity="success">
         <AlertTitle>Success</AlertTitle>
-        You have successfully made an Order —{" "}
-        <strong>Congrats on Making your Purchase</strong>
+        You have successfully made an Order - {" "}
+        <strong>Congrats on Making your purchase</strong>
       </Alert>
     </Box>
   );
